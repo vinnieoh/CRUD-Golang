36a01d0305bbf914341489ee8d29b758c7d78f0c
Vinicius Oliveira <viniciusoliveira926@gmail.com>
2023-05-08 22:12:02 -0300

    feat: add model user

--- app/models/modelUser.go
+++ app/models/modelUser.go
@@ -1,22 +1,12 @@
 package models
 
 type User struct {
-	ID       int64  `json:"id"`
-	Nome     string `json:"nome"`
-	Telefone string `json:"telefone"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	ID        int64  `json:"id"`
+	Nome      string `json:"nome"`
+	Telefone  string `json:"telefone"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+	Birthday  string `json:"birthday"`
+	CreatedAt string `json:"createdAt"`
+	UpdatedAt string `json:"updatedAt"`
 }
-
-/*
-typer User struct{
-	ID int64 `json:"id"`
-	Nome string `json:"nome"`
-	Telefone string `json:"telefone"`
-	Birthday Date
-	Email string `json:"email"`
-	Password string `json:"password"`
-	createdAt
-  	updatedAt
-}
-*/
